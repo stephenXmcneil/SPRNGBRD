aad7e63e108767435f26f069c25578e8fea7c39a
stephenxmcneil <stephenXmcneil@gmail.com>
2021-04-23 15:50:58 -0400

    Adding reduce exercises

--- js-array-reduce/reduce.js
+++ js-array-reduce/reduce.js
@@ -61,12 +61,19 @@ Examples:
 */
 
 function addKeyAndValue(arr, key, value) {
-
-    return arr.map();
+    return arr.reduce(function(entry, next, idx){
+        // entry[key] = value;
+        // console.log( entry[key]);
+        //console.log(idx);
+        entry[idx][key] = value;
+        return entry;
+    },arr);
 }
 
 /*
-Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
+Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run 
+the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in 
+the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
 
 Examples:
     
@@ -87,4 +94,14 @@ Examples:
     partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
 */
 
-function partition(arr, callback) {}
+function partition(arr, callback) {
+    //const newArr = [ [], []]
+    return arr.reduce(function(acc, nextVal){ 
+        if(callback(acc)){
+            acc[0].push(nextVal);
+        }else{
+            acc[1].push(nextVal);
+        }
+        return acc;
+    }, [[],[]]);
+}
