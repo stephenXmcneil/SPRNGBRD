7759ce6352e34b1adbbf9454200ac2a4cf52e3c0
stephenxmcneil <stephenXmcneil@gmail.com>
2021-07-30 17:42:27 -0400

    adding jeopardy file

---
+++ section-1/jeopardy/jeopardy.test.js
@@ -0,0 +1,11 @@
+describe('getCategory()', function () {
+  it('should populate obj', function () {
+    const arr = [{ title: 'color' }, { title: 'food' }, { title: 'materials' }]
+
+    expect(getCategory(arr)).toBe('['color', 'food', 'materials']')
+  })
+
+  it('should', function () {
+
+  })
+})
